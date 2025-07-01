9531b694636b5c481327b08aa5c6f6de0df3e672
saasipro <saasifypro@gmail.com>
2025-07-01 21:22:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star KHAN-MD
+Don't Forget To Give Star DARKZONE-MD
 
  Server running on http://localhost:` + PORT)
 })
